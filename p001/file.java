1bb0982ba6feffe0f1892e22861094e3ffd9926a
Yacha Venkata Rakesh <venkatarakesh1234y@gmail.com>
2020-05-10 20:29:09 +0530

    added q1 java file

--- p001/file.java
+++ p001/file.java
@@ -1,4 +1,13 @@
 import java.util.Scanner;
-public static void main(string args[]){
-    
+public class file{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        int t = input.nextInt();
+        for(int i=0;i<t;i++){
+            long n = input.nextInt();
+            n--;
+            long a=n/3,b=n/5,c=n/15;
+            System.out.println(((a*(a+1)*3-c*(c+1)*15+b*(b+1)*5)>>1));
+        }
+    }
 }
